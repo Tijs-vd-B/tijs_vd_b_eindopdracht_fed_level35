53b8360c85ec956880ad1cdec94ff584a444fcbe
Tijs-vd-B <81232322+Tijs-vd-B@users.noreply.github.com>
2021-06-04 01:49:32 +0200

    Sorting added on all headers
    - ascending and descending
    -  using React.useState() hook to set sort-order.
    -  Sorted data is put in a React.useMemo() hook

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,19 +1,38 @@
 import React from "react";
 
 function SongList(props) {
-  const [sortedField, setSortedField] = React.useState(null);
-  let sortedData = [...props.songs];
-  if (sortedField !== null) {
-    sortedData.sort((a, b) => {
-      if (a[sortedField] < b[sortedField]) {
-        return -1;
-      }
-      if (a[sortedField] > b[sortedField]) {
-        return 1;
-      }
-      return 0;
-    });
-  }
+  const data = props.songs;
+  const [sortConfig, setSortConfig] = React.useState(null);
+
+  const sortedData = React.useMemo(() => {
+    let sortedData = [...data];
+    if (sortConfig !== null) {
+      sortedData.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sortedData;
+  }, [data, sortConfig]);
+
+  const requestSort = (key) => {
+    let direction = "ascending";
+    if (
+      sortConfig &&
+      sortConfig.key === key &&
+      sortConfig.direction === "ascending"
+    ) {
+      direction = "descending";
+    }
+    setSortConfig({ key, direction });
+    console.log(sortConfig);
+  };
+
   const listItems = sortedData.map((item) => (
     <tr className="song-item" key={item.id} value={item.title}>
       <td>{item.title}</td>
@@ -33,17 +52,25 @@ function SongList(props) {
         <thead>
           <tr className="song-header">
             <th className="song-row__item">
-              <button type="button" onClick={() => setSortedField("title")}>
+              <button type="button" onClick={() => requestSort("title")}>
                 Song
               </button>
             </th>
             <th className="song-row__item">
-              <button type="button" onClick={() => setSortedField("artist")}>
+              <button type="button" onClick={() => requestSort("artist")}>
                 Artist
               </button>
             </th>
-            <th className="song-row__item">Genre</th>
-            <th className="song-row__item">Rating</th>
+            <th className="song-row__item">
+              <button type="button" onClick={() => requestSort("genre")}>
+                Genre
+              </button>
+            </th>
+            <th className="song-row__item">
+              <button type="button" onClick={() => requestSort("rating")}>
+                Rating
+              </button>
+            </th>
             <th className="song-row__item">Remove</th>
           </tr>
         </thead>
